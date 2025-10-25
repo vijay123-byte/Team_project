297500e1c69ccd895538ea4b6b9adb06bb568b8b
vijay bujjigalla <you@example.com>
2025-10-25 23:19:43 +0530

    this react

---
+++ app.js
@@ -0,0 +1,29 @@
+ 
+ /*<div id="parent">
+      <div id="child">
+          <h1>I'm h1 tag</h1>
+          <h2>I'm h2 tag </h2>
+      </div>
+ </div>
+*/
+
+ const heading=React.createElement("div",
+                        {id:"parent"},
+                        React.createElement("div",{id:"child"},
+                            [React.createElement("h1",{},"I'm h1 tag"),
+                                React.createElement("h2",{},"I'm h2 tag")
+                            ]
+                        )
+                );
+ const root=ReactDOM.createRoot(document.getElementById("root"));
+ root.render(heading);
+
+
+
+ 
+
+
+
+
+
+
